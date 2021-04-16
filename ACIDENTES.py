38420e1ed7a5c032b2f08a2865bd5c7d2fd44c58
Natanaelvie <82156265+Natanaelvie@users.noreply.github.com>
2021-04-15 23:59:33 -0300

    Update ACIDENTES.py

--- ACIDENTES.py
+++ ACIDENTES.py
@@ -5,9 +5,9 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 
-tes = pd.read_excel(r"estados.xlsx")
-dados = tes.values
-ano = dados[:,0]
+tes = pd.read_excel(r"estados.xlsx") #pega a planilha do execel
+dados = tes.values                   #transforma a planilha em uma lista
+ano = dados[:,0]                     #variáveis que selecionam a lista e escolhe uma coluna
 SP = dados[:,1]
 RS = dados[:,2]
 MT = dados[:,3]
@@ -37,20 +37,23 @@ INDEFINIDO = dados[:,26]
 PB = dados[:,27]
 RN = dados[:,28]
 
+#variável que define os nomes que vai ser usado na caixa seletora
 opcoes = ["SP", "RS", "MT", "PA", "PR", "MG", "GO", "MS", "AM", "BA", "SC", "MA", "RR", "RJ","TO", "PI", "PE", "CE", "AC", "ES", "AL", "AP", "RO", "SE", "DF", "INDEFINIDO", "PB", "RN"]
 
+#variáveis que definem o nomes e os valores que serão usados no filtro
 nomes_maior_menor_50 = ["Maior que 50%", "Menor que 50%", "Total"]
 valor_de_maior_menor_50 = [">50", "<50", "total"]
 
 
 
-tes = pd.read_excel(r"outros.xlsx")
-dados = tes.values
-categoria_1 = dados[:,0]
+tes = pd.read_excel(r"outros.xlsx")  #pega a planilha do execel
+dados = tes.values                   #transforma a planilha em uma lista
+categoria_1 = dados[:,0]             #variáveis que selecionam a lista e escolhe uma coluna
 valor_1 = dados[:,1]
 categoria_2 = dados[:,2]
 valor_2 = dados[:,3]
 
+#criação dos gráficos de pizza
 fig_1 = go.Figure()
 fig_1.add_trace(go.Pie(values = valor_1, labels = categoria_1))
 fig_1.update_layout(title='ACIDENTES')
@@ -60,30 +63,30 @@ fig_2.add_trace(go.Pie(values = valor_2, labels = categoria_2))
 fig_2.update_layout(title='INCIDENTES GRAVES')
 
 
-
+#aqui vai ser definido as configurações do grafico de barra, caixa seletora e filtro
 app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Dropdown( 
-        id = "filtro",
-        options = [{'label': nome, 'value':nome} for nome in opcoes], 
-        value = "SP",
+        id = "filtro",                                                 #caixa seletora
+        options = [{'label': nome, 'value':nome} for nome in opcoes],  #os nomes da caixa seletora será tirado da variável opcoes
+        value = "SP",                                                  #nome da primeira opção
         clearable = False
     ),
     dcc.RadioItems( 
-        id = "filtro_2",
-        options = [{'label': nome, 'value':valor} for nome, valor in zip(nomes_maior_menor_50, valor_de_maior_menor_50)], 
-        value = "total", 
+        id = "filtro_2",                                                                                                  #filtro
+        options = [{'label': nome, 'value':valor} for nome, valor in zip(nomes_maior_menor_50, valor_de_maior_menor_50)], #as opções de nomes e valores tirados das variáveis nomes_maior_menor_50 e valor_de_maior_menor_50
+        value = "total",                                                                                                  #primeiro valor
     ),
-    dcc.Graph(id = "grafico"), 
+    dcc.Graph(id = "grafico"),  #execução dos três gráficos
     dcc.Graph(figure = fig_1),
     dcc.Graph(figure = fig_2)
 ])
 
-
-def separa_dados(nome, valor):
-    porcentagens_filtradas = []
-    meses_filtrados = []
-    if nome == "SP":
+#aqui será programado as condições para a mudança de dados (através da caixa seletora e filtro)
+def separa_dados(nome, valor):    
+    porcentagens_filtradas = []     #mudanças para o filtro - relativo ao eixo x
+    meses_filtrados = []            #mudanças para o filtro - relativo ao eixo y
+    if nome == "SP":                #mudanças do eixo y de acordo com o nome selecionado na caixa seletora
         y = SP
     if nome == "RS":
         y = RS
@@ -141,27 +144,29 @@ def separa_dados(nome, valor):
         y = RN
 
 
-    if valor == ">50":
-        for porcentagem, data in zip(y, ano):
-            if porcentagem >= 50:
-                porcentagens_filtradas.append(porcentagem)
+    if valor == ">50":                                           #no filtro, quando selecionar >50
+        for porcentagem, data in zip(y, ano):                    #porcentagem = y e data = ano
+            if porcentagem >= 50:                                #para y >= 50 ...
+                porcentagens_filtradas.append(porcentagem)       #mostrar os selecionados
                 meses_filtrados.append(data)
     elif valor == "<50":
         for porcentagem, data in zip(y, ano):
             if porcentagem <= 50:
                 porcentagens_filtradas.append(porcentagem)
                 meses_filtrados.append(data)
-    elif valor == "total":
+    elif valor == "total":                                        #se for "total", mostrar todos os dados
         porcentagens_filtradas = y
         meses_filtrados = ano
-    return [porcentagens_filtradas, meses_filtrados]
+    return [porcentagens_filtradas, meses_filtrados]              #repati os comandos
 
 @app.callback(
-    Output(component_id='grafico', component_property= 'figure'),
-    Input(component_id='filtro', component_property='value'),
-    Input(component_id='filtro_2', component_property='value') 
+    Output(component_id='grafico', component_property= 'figure'),  #exibir gráficos e figuras
+    Input(component_id='filtro', component_property='value'),      #recebe comandos para a mudança da caixa seletora
+    Input(component_id='filtro_2', component_property='value')     #recebe comandos para a mudança do filtro
 )
 
+
+#definição do gráfico de acordo com so comandos
 def grafico_1(argumento, maior_menor): 
     dados_filtrados = separa_dados(argumento, maior_menor) 
     eixo_x = dados_filtrados[1]
@@ -173,9 +178,8 @@ def grafico_1(argumento, maior_menor):
        
     )
 
-    grafico = go.Figure(linha1) 
-
-    grafico.update_layout( 
+    grafico = go.Figure(linha1)                    #criação do gráfico de barras
+    grafico.update_layout(                         #estética do gráfico
         title = dict( 
             text = "Acidentes Estado x anos", 
             font = dict( 
@@ -190,7 +194,7 @@ def grafico_1(argumento, maior_menor):
         paper_bgcolor = "black", 
         plot_bgcolor = "snow", 
     )
-    return grafico
+    return grafico                                 #repetição dos comandos para o gráfico de barras
 
 
 if __name__ == '__main__':
