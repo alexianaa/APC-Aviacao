fd2f60caaf7f39c70261174655a105771865cc2e
Philipe Silva <philipe.rsm@gmail.com>
2021-04-17 20:46:01 -0400

    atualização de alguns nomes de variáveis, organização do código e melhoria no layout geral

--- esboço.py
+++ esboço.py
@@ -6,12 +6,12 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 
-
-#abrindo o arquivo de aeromodelos
-
+################################################################## MODELO DE AERONAVES ##################################################################
+# Abertura e leitura da base de dados de aernonaves
 arquivo_csv = open('database_aeromodelos.csv', newline='')
 dados = csv.DictReader(arquivo_csv, delimiter = ',')
 
+# Criação das listas de características que receberão os dados 
 modelo = []
 peso_decolagem = []
 comprimento_pista = []
@@ -25,6 +25,7 @@ comprimento_aeronave = []
 empenagem = []
 passageiros = []
 
+# Atribuição dos dados às listas
 for dado in dados:
     modelo.append(dado['modelo_aeronave'])
     peso_decolagem.append(float(dado['peso_decolagem']))
@@ -39,11 +40,14 @@ for dado in dados:
     empenagem.append(float(dado['empenagem']))
     passageiros.append(int(dado['capacidade_de_passageiros']))
 
-#abrindo os arquivos de acidentes
-    
+
+################################################################## ACIDENTES AÉREOS ##################################################################
+# Leitura da base de dados de acidentes aéreos
 tes = pd.read_excel(r"estados.xlsx")
 tes = pd.read_excel(r"estados.xlsx")
 dados = tes.values
+
+# Criação de listas das Unidades Federativas e atribuição de dados às listas 
 ano = dados[:,0]
 SP = dados[:,1]
 RS = dados[:,2]
@@ -74,16 +78,22 @@ INDEFINIDO = dados[:,26]
 PB = dados[:,27]
 RN = dados[:,28]
 
+# Leitura da base de dados da natureza e categoria dos acidentes
 tes = pd.read_excel(r"outros.xlsx")
 dados = tes.values
+
+# Criação das listas e atribuição de dados às listas
 categoria_1 = dados[:,0]
 valor_1 = dados[:,1]
 categoria_2 = dados[:,2]
 valor_2 = dados[:,3]
 
-#abrindo os arquivos de fluxo de viagens
 
+################################################################## FLUXO DE PASSAGEIROS E DECOLAGENS ##################################################################
+# Leitura da base de dados de fluxo de passageiros
 df_passageiros = pd.read_csv('Fluxo - Passageiros.csv', quotechar='"') 
+
+# Criação das listas de fluxo de passageiros por empresas e atribuição de dados às listas
 data = df_passageiros.values[:,0] 
 gol_passageiros= df_passageiros.values[:,1]
 tam_passageiros = df_passageiros.values[:,2]
@@ -91,22 +101,30 @@ azul_passageiros = df_passageiros.values[:,3]
 avianca_passageiros = df_passageiros.values[:,4]
 passaredo_passageiros = df_passageiros.values[:,5]
 
+# Leitura da base de dados de fluxo de decolagens
 df_decolagens = pd.read_csv('Fluxo - Decolagens.csv', quotechar='"') 
+
+# Criação das listas de fluxo de decolagens por empresas e atribuição de dados às listas
 gol_decolagens = df_decolagens.values[:,1]
 tam_decolagens = df_decolagens.values[:,2]
 azul_decolagens = df_decolagens.values[:,3]
 avianca_decolagens = df_decolagens.values[:,4]
 passaredo_decolagens = df_decolagens.values[:,5]
 
-df_passageiros_por_decolagem = pd.read_csv('Fluxo - Passageiros por Decolagem.csv', quotechar='"') 
+# Leitura da base de dados do fluxo de passageiros por decolagens
+df_passageiros_por_decolagem = pd.read_csv('Fluxo - Passageiros por Decolagem.csv', quotechar='"')
+
+#Criação das listas de fluxo de passageiros por decolagem por empresas e atribuição de dados às listas
 gol_passageiros_por_decolagem = df_passageiros_por_decolagem.values[:,1]
 tam_passageiros_por_decolagem = df_passageiros_por_decolagem.values[:,2]
 azul_passageiros_por_decolagem = df_passageiros_por_decolagem.values[:,3]
 avianca_passageiros_por_decolagem = df_passageiros_por_decolagem.values[:,4]
 passaredo_passageiros_por_decolagem = df_passageiros_por_decolagem.values[:,5]
 
-#filtros de aeromodelos
 
+################################################################## FILTRAGEM DE DADOS -> LISTAS DE METADADOS ##################################################################
+# Metadados de Modelo de Aeronaves
+#   -> características das aeronaves
 opcoes_filtro = ['Peso de decolagem (kg)',
                 'Comprimento de pista (m)',
                 'Velocidade de aproximação em nós',
@@ -119,68 +137,73 @@ opcoes_filtro = ['Peso de decolagem (kg)',
                 'Empenagem (m)',
                 'Capacidade de passageiros']
 
+#   -> ampliação de valores
+opcoes_de_ampliacao = ['Capacidade de passageiros menor que 150 assentos', 'Peso de decolagem menor que 65 mil kg', 'Total']
+valor_de_ampliacao = ['<150', '<65000', 'Total']
 
-opcoes_zomm_peso = ['Menor que 150', 'Menor que 65 mil', 'Total']
-valor_de_opcoes_zomm_peso = ['<150', '<65000', 'Total']
-
-#filtros de acidentes
-
+# Metadados de Acidentes Aéreos
+#   -> Unidades da Federação
 opcoes = ["SP", "RS", "MT", "PA", "PR", "MG", "GO", "MS", "AM", "BA", "SC", "MA", "RR", "RJ","TO", "PI", "PE", "CE", "AC", "ES", "AL", "AP", "RO", "SE", "DF", "INDEFINIDO", "PB", "RN"]
 
+#   -> ampliação de valores
 nomes_maior_menor_50 = ["Maior que 50%", "Menor que 50%", "Total"]
 valor_de_maior_menor_50 = [">50", "<50", "total"]
 
-#criação do grafico de acidentes
 
-fig_1 = go.Figure()
-fig_1.add_trace(go.Pie(values = valor_1, labels = categoria_1))
-fig_1.update_layout(title='ACIDENTES')
-
-fig_2 = go.Figure()
-fig_2.add_trace(go.Pie(values = valor_2, labels = categoria_2))
-fig_2.update_layout(title='INCIDENTES GRAVES')
-
-#criacao do grafico de fluxo
-
+################################################################## CRIANDO GRÁFICOS ##################################################################
+# criação do gráfico de fluxo de passageiros
 fig_passageiros = go.Figure()
 fig_passageiros.add_trace(go.Scatter(x = data, y = gol_passageiros, mode='lines', name= 'GOL', line=dict(color="orange")))
 fig_passageiros.add_trace(go.Scatter(x = data, y = tam_passageiros, mode='lines', name= 'TAM', line=dict(color="red")))
 fig_passageiros.add_trace(go.Scatter(x = data, y = azul_passageiros, mode='lines', name= 'AZUL', line=dict(color="blue")))
 fig_passageiros.add_trace(go.Scatter(x = data, y = avianca_passageiros, mode='lines', name= 'AVIANCA', line=dict(color="darkred")))
 fig_passageiros.add_trace(go.Scatter(x = data, y = passaredo_passageiros, mode='lines', name= 'PASSAREDO', line=dict(color="yellow")))
-fig_passageiros.update_layout(title='PASSAGEIROS',
+fig_passageiros.update_layout(title='FLUXO DE PASSAGEIROS',
                      xaxis_title='Mês', 
                      yaxis_title='Número de Passageiros')
 
+# criação do gráfico de fluxo de decolagens
 fig_decolagens = go.Figure() 
 fig_decolagens.add_trace(go.Scatter(x = data, y = gol_decolagens, mode='lines', name= 'GOL', line=dict(color="orange")))
 fig_decolagens.add_trace(go.Scatter(x = data, y = tam_decolagens, mode='lines', name= 'TAM', line=dict(color="red")))
 fig_decolagens.add_trace(go.Scatter(x = data, y = azul_decolagens, mode='lines', name= 'AZUL', line=dict(color="blue")))
 fig_decolagens.add_trace(go.Scatter(x = data, y = avianca_decolagens, mode='lines', name= 'AVIANCA', line=dict(color="darkred")))
 fig_decolagens.add_trace(go.Scatter(x = data, y = passaredo_decolagens, mode='lines', name= 'PASSAREDO', line=dict(color="yellow")))
-fig_decolagens.update_layout(title='DECOLAGENS',
+fig_decolagens.update_layout(title='FLUXO DE DECOLAGENS',
                      xaxis_title='Mês', 
                      yaxis_title='Número de Decolagens')
 
+# criação do gráfico de fluxo de passageiros por decolagens
 fig_passageiros_por_decolagem = go.Figure() 
 fig_passageiros_por_decolagem.add_trace(go.Scatter(x = data, y = gol_passageiros_por_decolagem, mode='lines', name= 'GOL', line=dict(color="orange")))
 fig_passageiros_por_decolagem.add_trace(go.Scatter(x = data, y = tam_passageiros_por_decolagem, mode='lines', name= 'TAM', line=dict(color="red")))
 fig_passageiros_por_decolagem.add_trace(go.Scatter(x = data, y = azul_passageiros_por_decolagem, mode='lines', name= 'AZUL', line=dict(color="blue")))
 fig_passageiros_por_decolagem.add_trace(go.Scatter(x = data, y = avianca_passageiros_por_decolagem, mode='lines', name= 'AVIANCA', line=dict(color="darkred")))
 fig_passageiros_por_decolagem.add_trace(go.Scatter(x = data, y = passaredo_passageiros_por_decolagem, mode='lines', name= 'PASSAREDO', line=dict(color="yellow")))
-fig_passageiros_por_decolagem.update_layout(title='PASSAGEIROS POR DECOLAGEM',
+fig_passageiros_por_decolagem.update_layout(title='FLUXO DE PASSAGEIROS POR DECOLAGEM',
                      xaxis_title='Mês', 
                      yaxis_title='Passageiros por Decolagem')
 
-#DASH
+# criação do gráficos da natureza e categoria de acidentes
+fig_1 = go.Figure()
+fig_1.add_trace(go.Pie(values = valor_1, labels = categoria_1))
+fig_1.update_layout(title='TIPOS DE ACIDENTES')
+
+fig_2 = go.Figure()
+fig_2.add_trace(go.Pie(values = valor_2, labels = categoria_2))
+fig_2.update_layout(title='INCIDENTES GRAVES')
+
+
+################################################################## DASH ##################################################################
 app = dash.Dash(__name__)
 
-
+# Configuração de Layout
 app.layout = html.Div([
-    # grafico de aeromodelo
-    html.P("Categoria:"),                       
+    #MODELOS DE AERONAVES
+    html.Div('Modelos de aeronaves e suas especificações técnicas', style={'color': 'grey', 'fontSize': 30}),
+    html.P("Categoria Técnica:"),                       
     dcc.Dropdown(                               
-        id="categoria-dropdown",               
+        id="dropdown-aeronaves",               
         options=[                               
             {'label': opcao, 'value': opcao}    
             for opcao in opcoes_filtro          
@@ -189,39 +212,40 @@ app.layout = html.Div([
         clearable=False,                        
     ),
     dcc.RadioItems(                            
-        id = "categoria-2",                   
-        options = [{'label': opcao, 'value':valor} for opcao, valor in zip(opcoes_zomm_peso, valor_de_opcoes_zomm_peso)],
+        id = "RadioItems-aeronaves",                   
+        options = [{'label': opcao, 'value':valor} for opcao, valor in zip(opcoes_de_ampliacao, valor_de_ampliacao)],
         value = 'Total'
     ),
-   
-   #colocando os graficos em ordem
-    dcc.Graph(id="bar-chart"),  #aeromodelos
-    
-    #grafico de acidentes
+    dcc.Graph(id="grafico-barras-aeronaves"),
+
+    # ACIDENTES AÉREOS OCORRIDOS POR ESTADO
+    html.Div('Acidentes aéreos', style={'color': 'grey', 'fontSize': 30}),
+    html.P("Unidade da Federação:"),
     dcc.Dropdown( 
-        id = "filtro",
+        id = "dropdown-acidentes",
         options = [{'label': nome, 'value':nome} for nome in opcoes], 
         value = "SP",
         clearable = False
     ),
     dcc.RadioItems( 
-        id = "filtro_2",
+        id = "RadioItems-acidentes",
         options = [{'label': nome, 'value':valor} for nome, valor in zip(nomes_maior_menor_50, valor_de_maior_menor_50)], 
         value = "total", 
     ),
-    
-    dcc.Graph(id = "grafico"), #acidentes
+    dcc.Graph(id = "grafico-barras-acidentesUF"), #acidentes
     dcc.Graph(figure = fig_1), #acidentes
     dcc.Graph(figure = fig_2), #acidentes
+
+    #FLUXO DE PASSAGEIROS
+    html.Div('Fluxo de passageiros e decolagens por empresa', style={'color': 'grey', 'fontSize': 30}),
     dcc.Graph(figure = fig_passageiros), #fluxo
     dcc.Graph(figure = fig_decolagens), #fluxo
     dcc.Graph(figure = fig_passageiros_por_decolagem), #fluxo
     
 ])
 
-#funcoes de aeromodelos
-
-def filtro(opcao, valor):
+# FUNÇÃO PARA FILTRAGEM DE DADOS NA CAIXA SELETORA DO GRÁFICO DE MODELOS DE AERONAVES
+def filtro_aeromodelos(opcao, valor):
     valores_filtrados = []
     aeronaves_filtradas = []
     if opcao == 'Peso de decolagem (kg)':
@@ -246,7 +270,6 @@ def filtro(opcao, valor):
         y = empenagem
     elif opcao == 'Capacidade de passageiros':
         y = passageiros
-
     if valor == '<150':
         for conteudo, aeromodelo in zip(y, modelo):
             if conteudo <= 150:
@@ -263,32 +286,8 @@ def filtro(opcao, valor):
 
     return [valores_filtrados, aeronaves_filtradas]
 
-#callback de aeromodelos
-
-@app.callback(
-    Output("bar-chart", "figure"), 
-    Input("categoria-dropdown", "value"),
-    Input('categoria-2', 'value')
-)
-
-# x e y de aeromodelos
-
-def grafico_1(argumento, maior_menor):
-    dados_filtrados = filtro(argumento, maior_menor)
-    eixo_x = dados_filtrados[1]
-    eixo_y = dados_filtrados[0]
-    barras1 = go.Bar(
-        x = eixo_x,
-        y = eixo_y,
-    )
-
-    grafico = go.Figure(barras1)
-
-    return grafico
-
-#funcoes de acidentes
-
-def separa_dados(nome, valor):
+# FUNÇÃO PARA FILTRAGEM DE DADOS NA CAIXA SELETORA DO GRÁFICO DE ACIDENTES AÉREOS POR ESTADO
+def filtro_acidentesUF(nome, valor):
     porcentagens_filtradas = []
     meses_filtrados = []
     if nome == "SP":
@@ -347,8 +346,6 @@ def separa_dados(nome, valor):
         y = PB
     if nome == "RN":
         y = RN
-
-
     if valor == ">50":
         for porcentagem, data in zip(y, ano):
             if porcentagem >= 50:
@@ -364,35 +361,68 @@ def separa_dados(nome, valor):
         meses_filtrados = ano
     return [porcentagens_filtradas, meses_filtrados]
 
-#callback de acidentes
+# CHAMANDO O DECORADOR DO GRÁFICO DE MODELOS DE AERONAVES
 @app.callback(
-    Output(component_id='grafico', component_property= 'figure'),
-    Input(component_id='filtro', component_property='value'),
-    Input(component_id='filtro_2', component_property='value') 
+    Output("grafico-barras-aeronaves", "figure"),
+    Input("dropdown-aeronaves", "value"),
+    Input('RadioItems-aeronaves', 'value')
 )
 
+# Definindo os valores de x e y para o gráfico de Modelos de Aeronaves
+def grafico_aeronaves(argumento, maior_menor):
+    dados_filtrados = filtro_aeromodelos(argumento, maior_menor)
+    eixo_x = dados_filtrados[1]
+    eixo_y = dados_filtrados[0]
 
-# x e y de acidentes
+    aeronaves_bar = go.Bar(
+        x = eixo_x,
+        y = eixo_y,
+    )
+
+    grafico_aeronaves_bar = go.Figure(aeronaves_bar)
+    grafico_aeronaves_bar.update_layout(
+        title = dict(
+            text = "Especificações técnicas por modelo de aeronave",
+            font = dict(
+                family = "Arial",
+                size = 20,
+                color = "grey"
+            ),
+            xref = "paper",
+            yref = "container",
+            x = 0.5,
+            y = 0.9
+        ),
+        plot_bgcolor = "snow",
+    )
+    return grafico_aeronaves_bar
+
+# CHAMANDO O DECORADOR DO GRÁFICO DE ACIDENTES AÉREOS POR ESTADOS
+@app.callback(
+    Output("grafico-barras-acidentesUF", "figure"),
+    Input("dropdown-acidentes", 'value'),
+    Input("RadioItems-acidentes", "value")
+)
+
+# Definindo os valores de x e y para o gráfico de Acidentes Aéros por Estado
 def grafico_1(argumento, maior_menor): 
-    dados_filtrados = separa_dados(argumento, maior_menor) 
+    dados_filtrados = filtro_acidentesUF(argumento, maior_menor) 
     eixo_x = dados_filtrados[1]
     eixo_y = dados_filtrados[0]
     
-    linha1 = go.Bar( 
+    acidentesUF_bar = go.Bar( 
         x = eixo_x,
         y = eixo_y,
-       
     )
 
-    grafico = go.Figure(linha1) 
-
-    grafico.update_layout( 
+    grafico_acidentesUF_bar = go.Figure(acidentesUF_bar) 
+    grafico_acidentesUF_bar.update_layout( 
         title = dict( 
-            text = "Acidentes Estado x anos", 
+            text = "Quantidade de acidentes x anos", 
             font = dict( 
                 family = "Arial",
-                size = 45,
-                color = "blue"),
+                size = 20,
+                color = "gray"),
             xref = "paper", 
             yref = "container", 
             x = 0.5, 
@@ -400,7 +430,8 @@ def grafico_1(argumento, maior_menor):
         ),
         plot_bgcolor = "snow", 
     )
-    return grafico
+    return grafico_acidentesUF_bar 
 
+# Executando a aplicação no servidor local
 if __name__ == '__main__':
     app.run_server(debug = True, use_reloader = False)
